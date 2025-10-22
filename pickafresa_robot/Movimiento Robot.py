4878ec2377df49688351e6d86c8bb7577c493607
Emiliano G <a01705488@tec.mx>
2025-10-22 16:25:02 -0600

    Simulación y conexion fisica

--- pickafresa_robot/Movimiento Robot.py
+++ pickafresa_robot/Movimiento Robot.py
@@ -1,18 +1,27 @@
 from robolink import *  # API RoboDK
+from robodk import *
+
 RDK = Robolink()
 RDK.setRunMode(RUNMODE_RUN_ROBOT)
+RDK.setRunMode(RUNMODE_SIMULATE)
 
 
-home_target = RDK.Item('Home', ITEM_TYPE_TARGET)
-foto_target = RDK.Item('Foto', ITEM_TYPE_TARGET)
-T5_target = RDK.Item('Target 5', ITEM_TYPE_TARGET)
+home_target = RDK.Item('Home', ITEM_TYPE_TARGET)# type: ignore
+foto_target = RDK.Item('Foto', ITEM_TYPE_TARGET)# type: ignore
+T3_target = RDK.Item('Target 3', ITEM_TYPE_TARGET)# type: ignore
+T4_target = RDK.Item('Target 4', ITEM_TYPE_TARGET)# type: ignore
+T5_target = RDK.Item('Target 5', ITEM_TYPE_TARGET)# type: ignore
 
 
-item = RDK.ItemUserPick('UR3e', ITEM_TYPE_ROBOT)
+item = RDK.ItemUserPick('UR3e', ITEM_TYPE_ROBOT)# type: ignore
 item.setSpeed(30, 30) 
 
 item.Connect()  # Intenta conexión activa (opcional)
 
+item.MoveJ(home_target)
 item.MoveJ(foto_target)
 item.MoveJ(T5_target)
+item.MoveJ(T3_target)
+item.MoveL(T4_target)
+item.MoveL(T3_target)
 item.MoveJ(home_target)
--- pickafresa_robot/Prueba_Conexion_API.py
+++ pickafresa_robot/Prueba_Conexion_API.py
@@ -1,16 +1,19 @@
 from robolink import *  # API RoboDK
+from robodk import *
 RDK = Robolink()
+RDK.setRunMode(RUNMODE_SIMULATE)
 RDK.setRunMode(RUNMODE_RUN_ROBOT)
 
 
-home_target = RDK.Item('Home', ITEM_TYPE_TARGET)
+home_target = RDK.Item('Home', ITEM_TYPE_TARGET) # type: ignore
 
-item = RDK.ItemUserPick('UR3e', ITEM_TYPE_ROBOT)
+item = RDK.ItemUserPick('UR3e', ITEM_TYPE_ROBOT)# type: ignore
 
 item.Connect()  # Intenta conexión activa (opcional)
 
+item.setSpeed(30, 30) 
 if item.Valid():
     print('Conectado correctamente con RoboDK.')
-    item.MoveJ(home_target)
+    item.MoveJ(home_target, blocking=False)
 else:
     print('No se pudo conectar con RoboDK.')
