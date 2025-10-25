5f86470f17e6e2624fa89c8de9325f927a3c67b3
Emiliano G <a01705488@tec.mx>
2025-10-24 18:45:18 -0600

    Continuacion programa

--- pickafresa_robot/Movimiento Robot.py
+++ pickafresa_robot/Movimiento Robot.py
@@ -1,23 +1,61 @@
 from robolink import *  # API RoboDK
 from robodk import *
+import paho.mqtt.client as mqtt
+import numpy as np
+import time
 
+
+#MQTT
+broker="192.168.9.1"
+topic="actuador"
+
+client = mqtt.Client()
+client.connect(broker, 1883, 60)
+
+#Encender o apagar actuador
+def actuador_on():
+    client.publish(topic, "1")
+    print("Actuador encendido")
+
+def actuador_off():
+    client.publish(topic, "0")
+    print("Actuador apagado")
+
+
+
+#Estado de robot: simulacion o fisico
 RDK = Robolink()
-RDK.setRunMode(RUNMODE_RUN_ROBOT)
+#RDK.setRunMode(RUNMODE_RUN_ROBOT)
 RDK.setRunMode(RUNMODE_SIMULATE)
 
-
+#Targets
 home_target = RDK.Item('Home', ITEM_TYPE_TARGET)# type: ignore
 foto_target = RDK.Item('Foto', ITEM_TYPE_TARGET)# type: ignore
 T3_target = RDK.Item('Target 3', ITEM_TYPE_TARGET)# type: ignore
 T4_target = RDK.Item('Target 4', ITEM_TYPE_TARGET)# type: ignore
 T5_target = RDK.Item('Target 5', ITEM_TYPE_TARGET)# type: ignore
 
-
+#Definir robot
 item = RDK.ItemUserPick('UR3e', ITEM_TYPE_ROBOT)# type: ignore
-item.setSpeed(30, 30) 
+item.setSpeed(60, 60) 
+#item.Connect()  # Intenta conexión activa (opcional)
 
-item.Connect()  # Intenta conexión activa (opcional)
 
+#Posición fresa respecto a camara
+fresa_cam=np.array()
+
+#Matriz de transformacion Cámara-Base robot
+cam_robot=np.array([
+    [0,0,0,0]
+    [0,0,0,0]
+    [0,0,0,0]
+    [0,0,0,0]
+])
+
+#Fresa en coordenadas del robot
+fresa_robot=cam_robot*fresa_cam
+
+#Rutina
 item.MoveJ(home_target)
 item.MoveJ(foto_target)
 item.MoveJ(T5_target)
--- pickafresa_robot/Prueba_Conexion_API.py
+++ pickafresa_robot/Prueba_Conexion_API.py
@@ -5,7 +5,11 @@ RDK.setRunMode(RUNMODE_SIMULATE)
 RDK.setRunMode(RUNMODE_RUN_ROBOT)
 
 
-home_target = RDK.Item('Home', ITEM_TYPE_TARGET) # type: ignore
+home_target = RDK.Item('Home', ITEM_TYPE_TARGET)# type: ignore
+foto_target = RDK.Item('Foto', ITEM_TYPE_TARGET)# type: ignore
+T3_target = RDK.Item('Target 3', ITEM_TYPE_TARGET)# type: ignore
+T4_target = RDK.Item('Target 4', ITEM_TYPE_TARGET)# type: ignore
+T5_target = RDK.Item('Target 5', ITEM_TYPE_TARGET)# type: ignore
 
 item = RDK.ItemUserPick('UR3e', ITEM_TYPE_ROBOT)# type: ignore
 
@@ -15,5 +19,11 @@ item.setSpeed(30, 30)
 if item.Valid():
     print('Conectado correctamente con RoboDK.')
     item.MoveJ(home_target, blocking=False)
+    item.MoveJ(foto_target)
+    item.MoveJ(T5_target)
+    item.MoveJ(T3_target)
+    item.MoveL(T4_target)
+    item.MoveL(T3_target)
+    item.MoveJ(home_target)
 else:
     print('No se pudo conectar con RoboDK.')
