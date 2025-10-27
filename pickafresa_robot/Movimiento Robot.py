000dc5395e4cafe05365734bd6efff0aa15cceaf
Emiliano G <a01705488@tec.mx>
2025-10-27 15:08:47 -0600

    Seguimineto rutina y activacion gripper

--- pickafresa_robot/Movimiento Robot.py
+++ pickafresa_robot/Movimiento Robot.py
@@ -28,17 +28,12 @@ RDK = Robolink()
 #RDK.setRunMode(RUNMODE_RUN_ROBOT)
 RDK.setRunMode(RUNMODE_SIMULATE)
 
-#Targets
-home_target = RDK.Item('Home', ITEM_TYPE_TARGET)# type: ignore
-foto_target = RDK.Item('Foto', ITEM_TYPE_TARGET)# type: ignore
-T3_target = RDK.Item('Target 3', ITEM_TYPE_TARGET)# type: ignore
-T4_target = RDK.Item('Target 4', ITEM_TYPE_TARGET)# type: ignore
-T5_target = RDK.Item('Target 5', ITEM_TYPE_TARGET)# type: ignore
-
 #Definir robot
 item = RDK.ItemUserPick('UR3e', ITEM_TYPE_ROBOT)# type: ignore
-item.setSpeed(60, 60) 
-#item.Connect()  # Intenta conexión activa (opcional)
+item.setSpeed(60, 60)
+
+# Intenta conexión activa (opcional)
+item.Connect() 
 
 
 #Posición fresa respecto a camara
@@ -46,20 +41,58 @@ fresa_cam=np.array()
 
 #Matriz de transformacion Cámara-Base robot
 cam_robot=np.array([
-    [0,0,0,0]
-    [0,0,0,0]
-    [0,0,0,0]
-    [0,0,0,0]
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
 ])
 
 #Fresa en coordenadas del robot
-fresa_robot=cam_robot*fresa_cam
+fresa_robot=cam_robot @ fresa_cam
+
+offset_x=-50
+
+matriz_offset=np.array([
+    [1,0,0,offset_x],
+    [0,1,0,0],
+    [0,0,1,0],
+    [0,0,0,1],
+])
+
+fresa_robot_offset= fresa_robot @ matriz_offset
+
+#Targets
+home_target = RDK.Item('Home', ITEM_TYPE_TARGET)# type: ignore
+foto_target = RDK.Item('Foto', ITEM_TYPE_TARGET)# type: ignore
+T3_target = RDK.Item('Target 3', ITEM_TYPE_TARGET)# type: ignore
+T4_target = RDK.Item('Target 4', ITEM_TYPE_TARGET)# type: ignore
+T5_target = RDK.Item('Target 5', ITEM_TYPE_TARGET)# type: ignore
+Debajo_fresa=Mat(fresa_robot_offset.tolist())
+Fresa=Mat(fresa_robot.tolist())
 
 #Rutina
-item.MoveJ(home_target)
-item.MoveJ(foto_target)
-item.MoveJ(T5_target)
-item.MoveJ(T3_target)
-item.MoveL(T4_target)
-item.MoveL(T3_target)
-item.MoveJ(home_target)
+while True:
+    if keyboard.is_pressed("esc"):
+        print("Ciclo Terminado")
+        actuador_off()
+        item.MoveJ(home_target)
+        break
+
+    item.MoveJ(home_target)
+    item.MoveJ(foto_target)
+    item.MoveJ(T5_target)
+    item.MoveJ(Debajo_fresa)
+    item.MoveL(Fresa)
+    time.sleep(2.0)
+    actuador_on()
+    time.sleep(2.0)
+    item.MoveL(Debajo_fresa)
+    item.MoveJ(T5_target)
+    item.MoveJ(T3_target)
+    item.MoveL(T4_target)
+    time.sleep(2.0)
+    actuador_off()
+    time.sleep(2.0)
+    item.MoveL(T3_target)
+    item.MoveJ(home_target)
+    time.sleep(2.0)
