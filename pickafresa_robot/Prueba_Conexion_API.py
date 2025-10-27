5dfef75c8fb90c673bdffacdba428101d52b4c05
Emiliano G <a01705488@tec.mx>
2025-10-27 16:30:20 -0600

    Mas pruebas

--- pickafresa_robot/Prueba_Conexion_API.py
+++ pickafresa_robot/Prueba_Conexion_API.py
@@ -6,6 +6,18 @@ RDK = Robolink()
 RDK.setRunMode(RUNMODE_SIMULATE)
 #RDK.setRunMode(RUNMODE_RUN_ROBOT)
 
+actuador_encendido=False
+
+def actuador_on():
+    global actuador_encendido
+    actuador_encendido=True
+    print("Actuador encendido")
+
+def actuador_off():
+    global actuador_encendido
+    actuador_encendido=False
+    print("Actuador apagado")
+
 
 home_target = RDK.Item('Home', ITEM_TYPE_TARGET)# type: ignore
 foto_target = RDK.Item('Foto', ITEM_TYPE_TARGET)# type: ignore
@@ -19,18 +31,24 @@ item.setSpeed(60, 60)
 #item.Connect()  # Intenta conexión activa (opcional)
 
 while True:
-    if keyboard.is_pressed("esc"):
+    if keyboard.is_pressed("esc") and not actuador_encendido:
         item.setSpeed(80, 80) 
         item.MoveJ(home_target)
         break
     if item.Valid():
         item.setSpeed(80, 80) 
         print('Conectado correctamente con RoboDK.')
-        item.MoveJ(home_target, blocking=False)
+        item.MoveJ(home_target)
         item.MoveJ(foto_target)
         item.MoveJ(T5_target)
+        time.sleep(2.0)
+        actuador_on()
+        time.sleep(2.0)
         item.MoveJ(T3_target)
         item.MoveL(T4_target)
+        time.sleep(2.0)
+        actuador_off()
+        time.sleep(2.0)
         item.MoveL(T3_target)
         item.MoveJ(home_target)
     else:
